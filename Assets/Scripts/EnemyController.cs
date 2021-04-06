68e352f7a8e46b801b8a8d535b4535f0bb6fcc12
LidorY <lidor323@gmail.com>
2021-04-06 22:28:19 +0300

    enemy spawning enemies moving towards the center

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+
+    public float speed;
+    private Vector3 Destination;
+    private Vector3 Origin;
+    private float DistanceFromDest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destination = new Vector3(0, 0.5f, 0);
+        DistanceFromDest = Vector3.Distance(transform.localPosition, Destination);
+        Origin = transform.localPosition;
+
+        StartCoroutine(moveObject());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator moveObject()
+	{
+        float toatlTime = DistanceFromDest / speed;
+        float currentTime = 0f;
+        while(Vector3.Distance(transform.localPosition, Destination) > 0)
+		{
+            currentTime += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(Origin, Destination, currentTime/ toatlTime);
+            yield return null;
+		}
+        Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+	public Transform enemyRef;
+	public float spawnDistance;
+
+	public float timeout;
+
+	private float timeCounter;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		timeCounter = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timeCounter += Time.deltaTime;
+		if (timeCounter > timeout)
+		{
+			float theta = Random.value * 360;
+			float x = spawnDistance * Mathf.Sin(theta);
+			float y = spawnDistance * Mathf.Cos(theta);
+			Vector3 spawn_position = new Vector3(x, 0.5f, y);
+
+
+			Instantiate(enemyRef, spawn_position, Quaternion.identity);
+			timeCounter = 0;
+		}
+	}
+
+}
+
